1ba9479faa753ae15a6dbfce4c3e8b14bbcadf52
SangJoon Lee(Jeremy) <sangjoon.jeremy.lee@gmail.com>
2021-06-09 01:11:02 +0800

    Initial Commit

---
+++ src/portfolioq7server/PortfolioQ7Server.java
@@ -0,0 +1,92 @@
+/**
+ * Portfolio Question 7
+ * SangJoon Lee
+ * 30024165
+ * 08/06/2021
+ */
+package portfolioq7server;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javax.swing.JOptionPane;
+
+public class PortfolioQ7Server extends Application {
+    private Stage stage;
+    private Scene scene;
+    private VBox vBox;
+    private Button openBtn;
+    private Button sendBtn;
+    private TextField textField;
+//    static Socket socket;
+    private InputStream is;
+    private BufferedOutputStream bos;
+    private File file;
+    static Server server = new Server();
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("File Transfer [SERVER]");
+        
+        // Buttons
+        Button startBtn = new Button("Start Server");
+        startBtn.setOnAction(e -> startButtonClicked());
+        
+        openBtn = new Button("Open File");
+        openBtn.setOnAction(e -> openButtonClicked());
+
+        sendBtn = new Button("Send File");
+        sendBtn.setOnAction(e -> sendButtonClicked());
+                
+        // TextField
+        textField = new TextField();
+        
+        vBox = new VBox();
+        vBox.setPadding(new Insets(10, 10, 10, 10));
+        vBox.setSpacing(10);
+        vBox.getChildren().addAll(startBtn, openBtn, textField, sendBtn);
+        
+        scene = new Scene(vBox, 300, 150);
+        stage.setScene(scene);
+        stage.show();
+        
+    }
+    public void startButtonClicked() {
+        try {
+            server.startServer();
+        } catch (IOException e) {
+            
+        }
+        
+    }
+    public void openButtonClicked() {
+        FileChooser fileChooser = new FileChooser();
+        file = fileChooser.showOpenDialog(stage);
+        if (file != null) {
+            textField.setText(file.getName());
+        }
+    }
+    
+    public void sendButtonClicked() {
+        try {
+            if (file != null) {
+                server.sendFile(file);
+            }
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Error: " + e);
+        }
+    }
+    
+    public static void main(String[] args) throws IOException {
+        launch(args);
+//        server.startServer();
+    }
+}
---
+++ src/portfolioq7server/Server.java
@@ -0,0 +1,105 @@
+package portfolioq7server;
+
+//import javafx.application.Application;
+//import javafx.stage.Stage;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+//import javafx.geometry.Insets;
+//import javafx.scene.Scene;
+//import javafx.scene.control.Button;
+//import javafx.scene.control.TextField;
+//import javafx.scene.layout.VBox;
+//import javafx.stage.FileChooser;
+import javax.swing.JOptionPane;
+
+public class Server extends Thread {
+//    private Stage stage;
+//    private Scene scene;
+//    private VBox vBox;
+//    private Button openBtn;
+//    private Button sendBtn;
+//    private TextField textField;
+    private Socket socket;
+    private InputStream is;
+    private BufferedOutputStream bos;
+    
+    public void startServer() throws IOException {
+        System.out.println("Server started");
+        try (ServerSocket serverSocket = new ServerSocket(9999)) {
+            socket = serverSocket.accept();
+//            run();
+            ReceiveThread rt = new ReceiveThread();
+            rt.setSocket(socket);
+            rt.setIs(is);
+            rt.setBos(bos);
+        }
+    }
+    @Override
+    public void run() {
+        try {
+//            ServerSocket serverSocket = new ServerSocket(9999);
+//            socket = serverSocket.accept();
+            byte[] fileBytes = new byte[1024];
+            String fileName = "data.csv";
+            BufferedReader buf = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String receivedString = buf.readLine();
+            String[] clientMsg = receivedString.split(",");
+            if (clientMsg[0].equals("fileName:")) {
+                fileName = clientMsg[1];
+            }
+
+            is = socket.getInputStream();
+            bos = new BufferedOutputStream(new FileOutputStream(fileName));
+
+            while (true) {
+                int bytesRead = is.read(fileBytes, 0, fileBytes.length);
+                bos.write(fileBytes, 0, bytesRead);
+                bos.flush();
+
+                if (bytesRead < fileBytes.length) {
+                    JOptionPane.showMessageDialog(null, "File received from Client");
+                }
+            }
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Error: " + e);
+        } finally {
+            try {
+                closeAll();
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(null, "Error: " + e);
+            }
+        }
+        
+    }
+    public void receiveFile(Socket socket) throws IOException {
+
+        
+    }
+    
+    public void sendFile(File file) throws IOException {
+        PrintWriter pw = new PrintWriter(socket.getOutputStream()); 
+        pw.println("fileName:," + file.getName());
+        pw.flush();
+        pw.close();
+        System.out.println("Send file");
+        System.out.println(file);
+        byte[] fileBytes = new byte[(int) file.length()];
+                
+        OutputStream os = socket.getOutputStream();
+        
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+        bis.read(fileBytes, 0, fileBytes.length);
+
+        os.write(fileBytes, 0, fileBytes.length);
+        os.flush();
+        os.close();
+        bis.close();
+    }
+    
+    public void closeAll() throws IOException {
+        bos.close();
+        is.close();
+        socket.close();
+    }    
+}
